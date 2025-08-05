06211b23b2b981b6990b64650b947e4519c65111
Erick <hearteric57@gmail.com>
2025-08-05 10:46:01 +0300

    chore: page states

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -8,50 +8,82 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         minimum: EdgeInsets.all(12),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          spacing: 4,
-          children: [
-            Icon(Icons.lock, size: 100),
-            SizedBox(height: 22),
-            Text(
-              "Please provide your username and password to continue",
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.titleSmall,
-            ),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            spacing: 4,
+            children: [
+              Icon(Icons.lock, size: 100),
+              SizedBox(height: 22),
+              Text(
+                "Please provide your username and password to continue",
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.titleSmall,
+              ),
 
-            SizedBox(height: 8),
+              SizedBox(height: 8),
 
-            TextFormField(
-              decoration: InputDecoration(
-                filled: true,
-                hintText: "Username",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(22),
-                  borderSide: BorderSide.none,
+              TextFormField(
+                controller: _usernameController,
+                validator: (value) {
+                  if ((value?.length ?? 0) < 3) {
+                    return "Please provide a valid username";
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  filled: true,
+                  hintText: "Username",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(22),
+                    borderSide: BorderSide.none,
+                  ),
                 ),
               ),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                filled: true,
-                hintText: "Password",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(22),
-                  borderSide: BorderSide.none,
+              TextFormField(
+                controller: _passwordController,
+                validator: (value) {
+                  if ((value?.length ?? 0) < 6) {
+                    return "Please provide a valid password, atleast 6 characters";
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  filled: true,
+                  hintText: "Password",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(22),
+                    borderSide: BorderSide.none,
+                  ),
                 ),
               ),
-            ),
 
-            SizedBox(height: 22),
-            FilledButton(onPressed: () {}, child: Text("Continue")),
-          ],
+              SizedBox(height: 22),
+              FilledButton(
+                onPressed: () {
+                  if (!formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text("Please correct the form and retry"),
+                        behavior: SnackBarBehavior.floating,
+                      ),
+                    );
+                  }
+                },
+                child: Text("Continue"),
+              ),
+            ],
+          ),
         ),
       ),
     );
